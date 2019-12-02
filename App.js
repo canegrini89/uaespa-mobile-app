d808f04d9667bc55ee76b432596d46be27c236e6
Ariel Negrini <canegrini89@gmail.com>
2019-12-02 15:42:20 -0300

    delete unnecesary screens

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#fff',
     justifyContent: 'center',
     marginTop: Constants.statusBarHeight
   }
