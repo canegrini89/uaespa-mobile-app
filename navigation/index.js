415715677e07ef22ee29fbdde31646474da33a0a
Emiliano Negrini <emi.negrini@gmail.com>
2019-12-10 11:50:21 -0300

    Home Screen with tabbar

--- navigation/index.js
+++ navigation/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-
+import { TextInput } from 'react-native';
 import { theme } from '../constants';
 import {
   FontAwesome,
@@ -11,6 +11,7 @@ import {
   MaterialCommunityIcons
 } from '@expo/vector-icons';
 
+import TabBarIcon from '../components/TabBarIcon';
 import LoginScreen from '../screens/LoginScreen';
 import SplashScreen from '../screens/SplashScreen';
 import ForgetPassword from '../screens/ForgetPasswordScreen';
@@ -21,16 +22,12 @@ import RegisterScreen from './../screens/RegisterScreen';
 import ForgetPasswordScreen from './../screens/ForgetPasswordScreen';
 import HomeScreen from './../screens/HomeScreen';
 import SavedScreen from './../screens/SavedScreen';
-import LoadingScreen from './../screens/LoadingScreen';
-import TabBarIcon from './../components/TabBarIcon';
 import NotificationReadScreen from './../screens/NotificationReadScreen';
 import MyProfile from './../screens/MyProfile';
 import HistoryUpComing from './../screens/HistoryUpComing';
 
-
 const AuthStackNavigator = createStackNavigator(
   {
-    HomeScreen,
     SplashScreen,
     ForgetPasswordSms,
     ChangePassword,
@@ -54,30 +51,36 @@ const AppTabNavigator = createBottomTabNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
-      tabBarIcon: () => (
-        <FontAwesome name='home' size={30} color={theme.colors.tertiary} />
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name='home' size={30} color={tintColor} />
       )
-    }
+    },
   },
   Saved: {
     screen: SavedScreen,
     navigationOptions: {
-      tabBarIcon: () => (
-        <Ionicons name='md-heart' size={30} color={theme.colors.tertiary} />
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name='md-heart' size={30} color={tintColor} />
       )
     }
   },
   History: {
+    navigationOptions:{
+      title: '',
+      tabBarIcon: () => (
+        <TabBarIcon />
+      )
+    },
     screen: HistoryUpComing
   },
   Alert: {
     screen: NotificationReadScreen,
     navigationOptions: {
-      tabBarIcon: () => (
+      tabBarIcon: ({ tintColor }) => (
         <MaterialCommunityIcons
           name='bell'
           size={30}
-          color={theme.colors.tertiary}
+          color={tintColor}
         />
       )
     }
@@ -85,12 +88,19 @@ const AppTabNavigator = createBottomTabNavigator({
   Profile: {
     screen: MyProfile,
     navigationOptions: {
-      tabBarIcon: () => (
-        <FontAwesome name='user' size={30} color={theme.colors.tertiary} />
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name='user' size={30} color={tintColor} />
       )
     }
+  },
+},
+{
+  tabBarOptions: {
+    activeTintColor: theme.colors.secondary,
+    inactiveTintColor: theme.colors.gray,
   }
 });
+
 
 const AppStackNavigator = createStackNavigator({
   AppTabNavigator: {
@@ -99,7 +109,7 @@ const AppStackNavigator = createStackNavigator({
       //   headerStyle: () => (
       //   )
       // }
-    }
+    },
   }
 });
 
